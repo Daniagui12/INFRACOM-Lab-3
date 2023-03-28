793750f2c06743695f5fd9143b6937a09545ad99
montejs3 <98852422+montejs3@users.noreply.github.com>
2023-03-27 19:50:24 -0500

    Update cliente_udp.py

--- Servidor-Cliente-UDP/Cliente/cliente_udp.py
+++ Servidor-Cliente-UDP/Cliente/cliente_udp.py
@@ -1,51 +1,64 @@
 import os
-import time
 import socket 
 import datetime
-
+import threading
 #Tamaño maximo de bytes de paquetes UDP
 bufferSize = 65535
 
-#Numero de clientes
-numClientes = 25
-
 #Puerto y Direccion IP del servidor 
-IP= '127.0.0.1'
-PUERTO= 65432
+server=('127.0.0.1',65432)
 
 #Crear un socket UDP
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-#Enviar un mensaje al servidor del archivo a solicitar
-fileName =input("Ingrese nombre del archivo ('archivo_100MB' o 'archivo_250MB') ")
-client_socket.sendto(fileName.encode('utf-8'), (IP, PUERTO))
+def solicitud(client_id,fileName):
+
+    #Enviar un mensaje al servidor del archivo a solicitar
+            #fileName =input("Ingrese nombre del archivo ('archivo_100MB' o 'archivo_250MB') ")
+    client_socket.sendto(fileName.encode('utf-8'), server)
 
 
-inicio_transferencia = datetime.datetime.now()
-# Recibir los fragmentos del archivo del servidor y guardarlos en un archivo local
-with open('Archivosrecibidos', 'wb') as f:
-    while True:
-        fragmento, direccion_servidor = client_socket.recvfrom(bufferSize)
-        if not fragmento:
-            break
-        f.write(fragmento)
+    inicio_transferencia = datetime.datetime.now()
+    # Recibir los fragmentos del archivo del servidor y guardarlos en un archivo local
+    with open('Archivosrecibidos', 'wb') as f:
+        while True:
+            fragmento, direccion_servidor = client_socket.recvfrom(bufferSize)
+            if not fragmento:
+                break
+            f.write(fragmento)
 
-# Detener temporizador
-fin_transferencia = datetime.datetime.now()
+    # Detener temporizador
+    fin_transferencia = datetime.datetime.now()
 
-# Calcular tiempo transcurrido
-tiempo = fin_transferencia - inicio_transferencia
+    # Calcular tiempo transcurrido
+    tiempo = fin_transferencia - inicio_transferencia
 
-# Obtener información del archivo recibido
-nombre_archivo = os.path.basename('Archivosrecibidos')
-tamaño_Archivo = os.path.getsize('Archivosrecibidos')
+    # Obtener información del archivo recibido
+    nombre_archivo = os.path.basename('Archivosrecibidos')
+    tamaño_Archivo = os.path.getsize('Archivosrecibidos')
 
 
-# Escribir registro en un archivo de registro
-with open('registro.txt', 'a') as f:
-    registro = f'Nombre del archivo :{nombre_archivo}\t Tamaño del archivo{tamaño_Archivo}\t  Total Tiempo{tiempo}\n'
-    f.write(registro)
+    # Escribir registro en un archivo de registro
+    with open('registro.txt', 'a') as f:
+        registro = f'Nombre del archivo :{nombre_archivo}\t Tamaño del archivo{tamaño_Archivo}\t  Total Tiempo{tiempo}\n'
+        f.write(registro)
 
+# Numero de clientes prueba
+numero_clientes = int(input("Ingrese el número de clientes para la prueba: "))
+
+# Nombre archivo a probar
+nombre_archivo_carga = input(input("Ingrese el nombre del archivo : "))
+
+
+threads = []
+for i in range(numero_clientes):
+    thread = threading.Thread(target=solicitud, args=(i,nombre_archivo_carga))
+    threads.append(thread)
+    thread.start()
+
+# Esperar a que todos los hilos terminen
+for thread in threads:
+    thread.join()
 
 #cerrar el socket 
 client_socket.close()
