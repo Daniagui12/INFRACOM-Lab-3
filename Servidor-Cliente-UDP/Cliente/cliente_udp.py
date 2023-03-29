ba23dcd54cb3f7c2f7b09e5b95bbd02811448178
Vanessa Martinez <l.martinezp@uniandes.edu.co>
2023-03-28 21:17:22 -0500

    cosas maricas de udp

--- Servidor-Cliente-UDP/Cliente/cliente_udp.py
+++ Servidor-Cliente-UDP/Cliente/cliente_udp.py
@@ -4,118 +4,50 @@ import logging
 from datetime import datetime
 import threading
 
-#Tamaño maximo de bytes de paquetes UDP
-bufferSize = 65535
+import socket
+import threading
+import time
 
-# Dirección IP y puerto del servidor
-server_address = ('157.253.220.113', 65432)
-
-# Crear un socket UDP
+buffeSize = 65507 
+numConexiones = 10
+CLIENTE_IP = '127.0.0.1'
+CLIENTE_PORT = 5000
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client_socket.connect((CLIENTE_IP, CLIENTE_PORT))
 
-# Establecer un tiempo de espera para recibir datos del servidor
-client_socket.settimeout(5)
+def send_file_request(filename, server_address):
+    client_socket.sendto(filename.encode(), server_address)
 
-# Directorio donde se van a almacenar los archivos recibidos
-directory = 'ArchivosRecibidos'
-
-# Directorio donde se van a almacenar los archivos de log
-log_directory = 'Logs'
-
-
-# Crear el directorio si no existe
-if not os.path.exists(directory):
-    os.makedirs(directory)
-
-# Crear el directorio de logs si no existe
-if not os.path.exists(log_directory):
-    os.makedirs(log_directory)
-
-# Configurar el logger para generar los archivos de log
-log_filename = datetime.now().strftime('%Y-%m-%d-%H-%M-%S-log.txt')
-log_path = os.path.join(log_directory, log_filename)
-logging.basicConfig(filename=log_path, level=logging.INFO,
-                    format='%(asctime)s - %(message)s')
-
-def solicitud(client_id,fileName,hilos):
-    try:
-        #Enviar un mensaje al servidor del archivo a solicitar
-        client_socket.sendto(fileName.encode('utf-8'), server_address)
-
-
-        filename=("Cliente"+str(client_id)+"-Prueba-"+str(hilos))
-
-
-        # Recibir los fragmentos del archivo del servidor y guardarlos en un archivo local
-        with open(os.path.join(directory,filename ), 'wt') as f:
-            start_time = datetime.now()
-            while True:
-                fragmento, address  = client_socket.recvfrom(bufferSize)
-                if not fragmento:
-                    break
-                f.write(fragmento)
-
-        # Detener temporizador
-        end_time = datetime.now()
-
-        transfer_time = end_time - start_time
-        log_message = f'Archivo {filename} recibido correctamente. ' \
-                    f'Tamaño: {os.path.getsize(os.path.join(directory, filename))} bytes. ' \
-                    f'Tiempo de transferencia: {transfer_time.total_seconds()} segundos.'
-        logging.info(log_message)
-
-        print('Archivo recibido correctamente')
-
-    except socket.timeout:
-        # Generar el registro del archivo de log en caso de que no se pueda recibir el archivo
-        log_message = f'No se pudo recibir el archivo {filename}.'
-        logging.info(log_message)
-
-
-
-# Numero de clientes prueba
-numero_clientes = int(input("Ingrese el número de clientes para la prueba: "))
-
-# Nombre archivo a probar
-nombre_archivo_carga = input("Ingrese el nombre del archivo : ")
-
-
-# Creacion de los hilos
-threads = []
-for i in range(numero_clientes):
-    thread = threading.Thread(target=solicitud, args=(i,nombre_archivo_carga,numero_clientes))
-    threads.append(thread)
-    thread.start()
-
-for thread in threads:
-    thread.join()
-
-
-#cerrar el socket 
-client_socket.close()
-
-
-
-"""
-def recibir(clientSocket, clientAddress, clientId, fileSize):
-    print(f'Receiving file from {clientAddress} (client {clientId})...')
-    tiempoInicio = time.time()
-    data = b''
-    while len(data) < fileSize:
-        packet = clientSocket.recv(bufferSize)
-        if not packet:
-            break
-        else:
-            data += packet
-    tiempoFin = time.time()
-    tiempoTranscurrido = tiempoFin - tiempoInicio
-    if len(data) == fileSize:
-        file_name = f'{clientId}-Prueba-{numClientes}.txt'
-        with open(os.path.join('ArchivosRecibidos', file_name), 'wb') as f:
+def receive_file(client_socket, file_size):
+    # Reescribimos el archivo en un archivo con el nombre "Cliente"
+    with open('ArchivosRecibidos/cliente.txt', 'wb') as f:
+        start_time = time.monotonic()
+        while file_size > 0:
+            data, _ = client_socket.recvfrom(buffeSize)
             f.write(data)
-        print(f'File received from {clientAddress} (client {clientId}): {file_name} ({fileSize} bytes, {tiempoTranscurrido:.2f} seconds)')
-        return True, tiempoTranscurrido
-    else:
-        print(f'Error receiving file from {clientAddress} (client {clientId})')
-        return False, None
-"""  
+            file_size -= len(data)
+        end_time = time.monotonic()
+    
+    # Calcular el tiempo total de recepción
+    tiempo_total = end_time - start_time
+
+def download_file(id, filename, server_address):
+    send_file_request(filename, server_address)
+    fileSize = client_socket.recvfrom(buffeSize)
+    receive_file(client_socket, int(fileSize))
+
+    client_socket.close()
+
+def main():
+    print("Starting client")
+    filename = input("Enter the file number to send (1 for 100MB or 2 for 250MB): ")
+    threads = []
+    for i in range(numConexiones):
+        t = threading.Thread(target=download_file, args=(i, filename, (CLIENTE_IP, CLIENTE_PORT)))
+        t.start()
+        threads.append(t)
+    for t in threads:
+        t.join()
+
+if __name__ == '__main__':
+    main()
--- Servidor-Cliente-UDP/Servidor/servidor_udp.py
+++ Servidor-Cliente-UDP/Servidor/servidor_udp.py
@@ -7,55 +7,91 @@ import threading
 import time
 
 buffeSize = 65507 
-file1 = 'archivo_100MB'
-file2 = 'archivo_250MB'
-numConexiones = 25
+file1 = "files/100MB.bin"
+file2 = "files/250MB.bin"
+SERVER_IP = '127.0.0.1'
+SERVER_PORT = 5000
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.bind((SERVER_IP, SERVER_PORT))
+threads = []
 
+def init_logger():
+    log_dir = 'Servidor-Cliente-UDP/Servidor/Logs'
+    if not os.path.exists(log_dir):
+        os.mkdir(log_dir)
 
-def enviar_archivo(connection, archivo, address):
-    # Enviar archivo por fragmentos
-    with open(archivo, 'rb') as f:
-        data = f.read(buffeSize)
-        start_time = time.monotonic()  # Tiempo de inicio del envío
-        while data:
-            connection.sendto(data, address)
+    log_filename = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + '-log.txt'
+    logging.basicConfig(filename=f'Servidor-Cliente-UDP/Servidor/Logs/{log_filename}', level=logging.INFO)
+    return logging.getLogger()
+
+logger = init_logger()
+
+def handle_client_request(file1, file2, filenum):
+    
+    # Recibir el nombre del archivo solicitado
+    print(f"Message received from client")
+    filenum = int(filenum)
+
+    # Enviar el archivo requerido
+    if filenum == 1:
+        file_size = os.path.getsize(file1)
+        print(f"Sending file size to client {file_size}")
+        # Se envia el file size
+        server_socket.sendto(str(file_size).encode(), (SERVER_IP, SERVER_PORT))
+        with open(file1, 'rb') as f:
             data = f.read(buffeSize)
-        end_time = time.monotonic()  # Tiempo de fin del envío
-    
-    # Calcular el tiempo total de envío
-    tiempo_total = end_time - start_time
-    
-    # Registrar en el archivo de log los detalles de la conexión
-    nombre_archivo = os.path.basename(archivo)
-    log_file = time.strftime('%Y-%m-%d-%H-%M-%S-log.txt', time.localtime())
-    with open('Logs/' + log_file, 'a') as f:
-        f.write(f'Archivo enviado: {nombre_archivo} ({os.path.getsize(archivo)} bytes)\n')
-        f.write(f'Tiempo de envío: {tiempo_total:.2f} segundos\n')
-        f.write(f'Conexión recibida desde: {address[0]}:{address[1]}\n\n')
+            start_time = time.monotonic()  # Tiempo de inicio del envío
+            while data:
+                server_socket.sendto(data, (SERVER_IP, SERVER_PORT))
+                data = f.read(buffeSize)
+            end_time = time.monotonic()  # Tiempo de fin del envío
+
+        # Calcular el tiempo total de envío
+        tiempo_total = end_time - start_time   
+
+        # Registrar en el archivo de log los detalles de la conexión
+        logger.info(f"Requested 100MB.bin")
+        logger.info(f"File succesfuly sent to client in {tiempo_total} with size {file_size} bytes")
+
+
+    # Enviar el archivo requerido
+    if filenum == 2:
+        file_size = os.path.getsize(file2)
+        print(f"Sending file size to client {file_size}")
+        with open(file2, 'rb') as f:
+            data = f.read(buffeSize)
+            start_time = time.monotonic()  # Tiempo de inicio del envío
+            while data:
+                server_socket.sendto(data, (SERVER_IP, SERVER_PORT))
+                data = f.read(buffeSize)
+            end_time = time.monotonic()  # Tiempo de fin del envío
+
+        # Calcular el tiempo total de envío
+        tiempo_total = end_time - start_time
+        
+        # Registrar en el archivo de log los detalles de la conexión
+        logger.info(f"Requested 250MB.bin")
+        logger.info(f"File succesfuly sent to client in {tiempo_total} with size {file_size} bytes")
+
+
+    else:
+        print('Invalid filename requested')
 
 def main():
-    # Crear el socket UDP y enlazarlo a un puerto específico
-    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udp_socket.bind(('127.0.0.1', 65432))
     
-    # Esperar a recibir conexiones entrantes
+    print("Starting server")
     while True:
-        address = udp_socket.recvfrom(buffeSize)
-        
-        # Identificar cual archivo desea el cliente
-        file = input("Enter the file number to send (1 for 100MB or 2 for file250MB): ")
-        if file == 1:
-            solicitud = file1
-        elif file == 2:
-            solicitud = file2
-        
-        
-        # Crear un thread para enviar el archivo
-        archivo = solicitud
-        thread = threading.Thread(target=enviar_archivo, args=(udp_socket, archivo, address))
-        thread.start()
+        filenum = server_socket.recv(buffeSize).decode()
+        t = threading.Thread(target=handle_client_request, args=(file1, file2, filenum))
+        t.start()
+        threads.append(t)
+    
+
 
 if __name__ == '__main__':
-    if not os.path.exists('Servidor-Cliente-UDP/Servidor/Logs'):
-        os.mkdir('Servidor-Cliente-UDP/Servidor/Logs')
     main()
+
+    for item in threads:
+        item.join()
+
+
