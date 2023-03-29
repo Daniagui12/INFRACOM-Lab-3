2dcc7722f169acb88f43c6d12e7b2cb008fc9590
Daniel Aguilera <d.aguilera@uniandes.edu.co>
2023-03-29 10:21:24 -0500

    paquetes llegando

--- Servidor-Cliente-UDP/Cliente/cliente_udp_thread.py
+++ Servidor-Cliente-UDP/Cliente/cliente_udp_thread.py
@@ -1,48 +1,29 @@
-from socket import socket, AF_INET, SOCK_DGRAM
-from threading import Thread
-import time
+import socket
+import threading
 
-SERVER_ADDRESS = ('127.0.0.1', 5000)
-NUM_CLIENTS = 25
+def send_message(id, host, port, message):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-class Client(Thread):
-    def __init__(self, id):
-        Thread.__init__(self)
-        self.id = id
+    # Send the file size we want to the server with a sequence number
+    sequence_number = 0
+    while True:
+        sequence_number += 1
+        data = f"{sequence_number}:{message}:{id}"
+        client_socket.sendto(data.encode(), (host, port))
+        response, server_address = client_socket.recvfrom(1024)
+        if response.decode() == f"ACK:{sequence_number}:{id}":
+            # Received acknowledgement for this packet, move on to the next one
+            print(f"Received acknowledgement for packet {sequence_number} from server at {server_address}")
+            break
 
-    def run(self):
-        s = socket(AF_INET, SOCK_DGRAM)
+    client_socket.close()
 
-        # Send client ID to server
-        s.sendto(str(self.id).encode(), SERVER_ADDRESS)
+if __name__ == '__main__':
+    host = 'localhost'
+    port = 8000
+    file_num = input("Enter the file number to send (1 for 100MB or 2 for 250MB): ")
 
-        # Receive file name message from server
-        file_name_msg, addr = s.recvfrom(1024)
-        print(f"Client {self.id}: Received file name message {file_name_msg.decode()}")
-
-        # Send 'Ready' message to server
-        s.sendto('Ready'.encode(), SERVER_ADDRESS)
-
-        # Receive file size from server
-        file_size_bytes, addr = s.recvfrom(1024)
-        file_size = int.from_bytes(file_size_bytes, byteorder='big')
-        print(f"Client {self.id}: Received file size {file_size}")
-
-        # Receive file data from server
-        with open(f"ArchivosRecibidos/client_{self.id}.txt", "wb") as f:
-            while True:
-                data, addr = s.recvfrom(1024)
-                if not data:
-                    break
-                f.write(data)
-
-        print(f"Client {self.id}: File received")
-
-        s.close()
-
-print("Starting clients")
-for i in range(NUM_CLIENTS):
-    client = Client(i)
-    client.start()
-
-print("All clients started")
+    # create 10 threads and send messages concurrently
+    for i in range(10):
+        thread = threading.Thread(target=send_message, args=(i, host, port, file_num))
+        thread.start()
--- Servidor-Cliente-UDP/Servidor/servidor_udp_thread.py
+++ Servidor-Cliente-UDP/Servidor/servidor_udp_thread.py
@@ -1,49 +1,39 @@
-from socket import socket, SOL_SOCKET, SO_REUSEADDR, AF_INET, SOCK_DGRAM
-import threading
-import os
-import struct
+import socket
 
-# Create a server UDP socket and allow address re-use
-s = socket(AF_INET, SOCK_DGRAM)
-s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-s.bind(('127.0.0.1', 5000))
+server_address = ('localhost', 8000)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.bind(server_address)
 
-class RequestHandler(threading.Thread):
-    def __init__(self, client_id, address):
-        threading.Thread.__init__(self)
-        self.client_id = client_id
-        self.address = address
-
-    def run(self):
-        # Send file name message to client
-        s.sendto('1'.encode(), self.address)
-
-        # Receive 'Ready' message from client
-        ready_msg_bytes, addr = s.recvfrom(1024)
-        ready_msg = ready_msg_bytes.decode()
-        print(f"Client {self.client_id}: {ready_msg}")
-
-        # Send file size to client
-        file_size = os.path.getsize("files/100MB.bin")
-        file_size_bytes = struct.pack("!Q", file_size)
-        s.sendto(file_size_bytes, self.address)
-
-        # Send file data to client
-        with open("files/100MB.bin", "rb") as f:
-            while True:
-                data = f.read(1024)
-                if not data:
-                    break
-                s.sendto(data, self.address)
-
-        print(f"Client {self.client_id}: File sent")
-
-print("Starting server")
 while True:
-    # Receive client ID from client
-    client_id_bytes, addr = s.recvfrom(1024)
-    client_id = int(client_id_bytes.decode())
+    # Create a dictionary to store received packets
+    received_packets = {}
+    client_id = None
 
-    # Start a new thread to handle the client request
-    request_handler = RequestHandler(client_id, addr)
-    request_handler.start()
+    while True:
+        # Receive a packet from the client
+        packet, client_address = server_socket.recvfrom(1024)
+
+        # Extract the client id and sequence number from the packet
+        parts = packet.decode().split(':', 2)
+        print(parts)
+        if len(parts) == 3:
+            client_id = int(parts[2])
+            sequence_number = int(parts[0])
+            message = parts[1]
+            print(f"Received packet {sequence_number} from client {client_id} at {client_address}: {message}")
+        else:
+            print(f"Received invalid packet from {client_address}: {packet.decode()}")
+            continue
+
+        # Store the packet in the dictionary
+        received_packets[sequence_number] = message
+
+        # Send an acknowledgement for this packet
+        server_socket.sendto(f"ACK:{sequence_number}:{client_id}".encode(), client_address)
+
+        # Check if we have received all packets
+        if len(received_packets) == 10:
+            # Reassemble the packets in order and print the result
+            message = ''.join(received_packets[i] for i in range(1, 11))
+            print(f"Received message from client {client_id} at {client_address}: {message}")
+            break
