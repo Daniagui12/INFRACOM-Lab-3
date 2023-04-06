db8a2630f67119bfac2a21a802978ebc39a05970
Daniel Aguilera <d.aguilera@uniandes.edu.co>
2023-04-06 15:47:18 -0500

    comments from english to spanish. done

--- Servidor-Cliente-TCP/Cliente/cliente_tcp_thread.py
+++ Servidor-Cliente-TCP/Cliente/cliente_tcp_thread.py
@@ -38,18 +38,19 @@ class ClientThread:
 
             if response_server == "ok":
                 self.s.send(message.encode())
+                print(f'Cliente {self.id} ha enviado el mensaje de preparado')
 
                 # Receive the padded file hash
                 file_hash_padded = self.s.recv(1024).decode()
 
                 # Strip any trailing null bytes from the received string
                 file_hash = file_hash_padded.rstrip('\x00')
-                print(f'Hash received from server for client {self.id}: {file_hash}')
+                print(f'Hash recibido del servidor por el cliente {self.id}: {file_hash}')
 
                 # Receive the file size
                 data = self.s.recv(8)
                 file_size = struct.unpack("!Q", data)[0]
-                print(f"File size received from server for client {self.id}: {file_size} bytes")
+                print(f"Tamanio del archivo recibido del servidor por el cliente {self.id}: {file_size} bytes")
 
                 self.receive_file(file_size, file_hash, num_clients)
                 
@@ -80,43 +81,43 @@ class ClientThread:
                 remaining_bytes -= len(data)
             end_time = datetime.datetime.now()
             time_diff = end_time - start_time
-            print(f"Received {file_size} bytes from server for client {client_id} in {time_diff.total_seconds()} seconds.")
+            print(f"Recibidos {file_size} bytes del servidor por el cliente {client_id} en {time_diff.total_seconds()} segundos.")
 
             # Send EOF message to server
             self.s.send("EOF".encode())
 
         # Calculate the hash of the received file
         calculated_hash = hashlib.md5(open(file_name, 'rb').read()).hexdigest()
-        print(f"Hash of received file for client {client_id}: {calculated_hash}")
+        print(f"Hash del archivo recibido por el cliente {client_id}: {calculated_hash}")
 
         if calculated_hash == file_hash:
-            print(f"Hash of received file matches the hash received from server for client {client_id}.")
+            print(f"Hash del archivo recibido es igual al hash recibido del servidor por el cliente {client_id}.")
             status = "SUCCESS"
         else:
-            print(f"Hash of received file does not match the hash received from server for client {client_id}.")
+            print(f"Hash del archivo recibido NO es igual al hash recibido del servidor por el cliente {client_id}.")
             status = "FAILED"
         with open(f"Logs/{self.log_filename}", 'a') as log_file:
-            log_file.write(f"Client {client_id+1}: file=Cliente{client_id+1}-Prueba-{num_clients}.txt, size={file_size}, status={status}, time={time_diff.total_seconds()} seconds\n")
+            log_file.write(f"Cliente {client_id+1}: Archivo=Cliente{client_id+1}-Prueba-{num_clients}.txt, Tamanio={file_size}, Estado={status}, puerto={self.s.getsockname()[1]}, tiempo={time_diff.total_seconds()} segundos\n")
 
 
 # Create a queue to hold the tasks for the worker threads
 q = Queue(maxsize=0)
 
-print("--Client app started--")
-num_clients = int(input("Enter the number of clients to request to the server (MAX = 25): "))
+print("--Client app iniciado--")
+num_clients = int(input("Ingrese el numero de clientes que solicitaran archivos al servidor (MAX = 25): "))
 
 # End the program if the number of clients is greater than 25
 if num_clients > 25:
-    print("The maximum number of clients is 25.")
+    print("El numero maximo de clientes es 25!")
     exit()
 
-file_num = input("Enter the file number to send (1 for 100MB or 2 for 250MB): ")
-# Function which generates a Client instance, getting the work item to be processed from the queue
+file_num = input("Ingrese el numero del archivo a enviar (1 para 100MB o 2 para 250MB): ")
+
 def worker():
     while True:
         try:
             item = q.get()
-            client = ClientThread(item, "127.0.0.1", 5000, datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + "-log.txt", file_num)
+            client = ClientThread(item, "192.168.1.125", 5000, datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + "-log.txt", file_num)
             client.run()
         except Exception as e:
             logging.exception(f"Exception in worker: {e}")
